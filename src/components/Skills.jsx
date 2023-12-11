b34051f178899dbbd48a2882e50de594a61a1810
pratyushkk <npratyush007@gmail.com>
2023-12-11 13:13:43 +0530

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -82,7 +82,7 @@ const Skills = () => {
               data={["Prototype", "Wireframe", "User Experience"]}
             /> */}
             <Service
-              num="4"
+              num="3"
               title="Android App Developement"
               // desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it"
               data={[
