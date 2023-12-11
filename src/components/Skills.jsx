d6df52c1d8824aad639b29c5ae1eaf3fb6cabf57
pratyushkk <npratyush007@gmail.com>
2023-12-11 13:10:55 +0530

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -50,8 +50,8 @@ const Skills = () => {
           <SkillIcon path={sk3} />
           {/*<SkillIcon path={sk4} /> */}
           <SkillIcon path={sk5} />
-          <SkillIcon path={sk6} />
-          <SkillIcon path={sk7} />
+          {/* <SkillIcon path={sk6} />
+          <SkillIcon path={sk7} /> */}
           <SkillIcon path={sk8} />
           <SkillIcon path={sk9} />
           <SkillIcon path={sk10} />
@@ -75,15 +75,15 @@ const Skills = () => {
               // desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it"
               data={["Prototype", "Wireframe", "User Experience"]}
             />
-            <Service
+            {/* <Service
               num="3"
               title="Wordpress Development"
               // desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it"
               data={["Prototype", "Wireframe", "User Experience"]}
-            />
+            /> */}
             <Service
               num="4"
-              title="Shopify Developement"
+              title="Android App Developement"
               // desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it"
               data={[
                 "Prototype",
